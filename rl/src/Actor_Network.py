010d3eaaa4917d4bd65f4fac1354b9e0145aa8f6
songqian2002 <songqian2002@gmail.com>
2018-03-17 19:07:35 -0400

    Network ready

--- rl/src/Actor_Network.py
+++ rl/src/Actor_Network.py
@@ -6,15 +6,16 @@ from keras.layers.merge import Add, Multiply
 from keras.optimizers import Adam
 import keras.backend as K
 import tensorflow as tf
+import pdb
 
 from Replay_Buffer import Replay_Buffer
 
 
 class Actor_Network(object):
-	def __init__(self, env, sess, batch_size=32, tau=0.125, learning_rate=0.001):
+	def __init__(self, env, sess, batch_size=32, tau=0.125, learning_rate=0.0001):
 		self.env = env
 		self.sess = sess
-		self.obs_dim = self.env.observation_space.shape
+		self.obs_dim = self.env.observation_space.shape[0]
 		self.act_dim = self.env.action_space.shape[0]
 
 		# hyperparameters
@@ -32,7 +33,7 @@ class Actor_Network(object):
 
 		# create model
 		self.model, self.weights, self.state = self.create_actor()
-		self.target_model,self.target_weights, self.create_actor_network()
+		self.target_model, self.target_weights, self.target_state = self.create_actor()
 
 		# gradients
 		self.action_gradient = tf.placeholder(tf.float32, [None, self.act_dim])
@@ -44,17 +45,14 @@ class Actor_Network(object):
 		self.sess.run(tf.initialize_all_variables())
 
 	def create_actor(self):
-
 		obs_in = Input(shape = [self.obs_dim])  # 3 states
+		# pdb.set_trace()
 
 		h1 = Dense(self.hidden_dim, activation = 'relu')(obs_in)
 		h2 = Dense(self.hidden_dim, activation = 'relu')(h1)
 		h3 = Dense(self.hidden_dim, activation = 'relu')(h2)
 
-		out = Dense(self.act_dim,  # 1 output
-					activation = 'tanh',
-					init = lambda shape,
-					name: normal(shape, scale=1e-4, name = name))(h3)
+		out = Dense(self.act_dim, activation='tanh')(h3)
 
 		model = Model(input = obs_in, output = out)
 
@@ -64,15 +62,15 @@ class Actor_Network(object):
 
 	def train(self, states, action_grads):
 		self.sess.run (self.optimize, 
-					feed_dict={self.state: states, self.action_gradients: action_grads})
+					feed_dict={self.state: states, self.action_gradient: action_grads})
 
 
 	def target_train(self):
 		actor_weights = self.model.get_weights()
-		actor_target_weights = self.target_model.get_Weights()
+		actor_target_weights = self.target_model.get_weights()
 
 		# update target network
-		for i in xrange(len(actor_weights)):
+		for i in range(len(actor_weights)):  # used to be xrange
 			actor_target_weights[i] = self.tau*actor_weights[i] + (1 - self.tau)*actor_target_weights[i]
 
 		self.target_model.set_weights(actor_target_weights)

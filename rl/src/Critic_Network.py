8c65e80d5b2d593618f2b77b050f1cfdd7cf6800
Rohit Murthy <rohitmur@andrew.cmu.edu>
2018-03-17 17:47:55 -0400

    Chris' A.C. classes, environment tested

---
+++ rl/src/Critic_Network.py
@@ -0,0 +1,72 @@
+import numpy as np
+import math
+from keras.initializations import normal, identity
+from keras.models import model_from_json, load_model
+from keras.engine.training import collect_trainable_weights
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Input, merge, Lambda, Activation
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+import keras.backend as K
+import tensorflow as tf
+
+
+class Critic_Network(object):
+	def __init__(self, env, sess, batch_size=32, tau=0.125, learning_rate=0.001):
+		self.sess = sess
+		self.bs = batch_size
+		self.obs_dim = self.env.observation_space.shape
+		self.act_dim = self.env.action_space.shape[0]
+
+		# hyperparameters
+		self.lr = learning_rate
+		self.bs = batch_size 
+		self.tau = tau
+		self.buffer_size = 5000
+		self.hidden_dim = 300
+
+		K.set_session(sess)
+
+		self.model, self.action, self.state = self.create_critic_network(self.obs_dim, self.act_dim)
+		self.target_model, self.target_action, self.target_state = self.create_critic_network(self.obs_dim, self.act_dim)
+		self.action_grads = tf.gradients (self.model.output, self.action)
+		self.sess.run(tf.initialize_all_variables())
+
+
+	def create_critic_network(self):
+
+		# parallel 1
+		state_input = Input(shape = [obs_dim])
+		w1 = Dense(self.hidden_dim, activation = 'relu')(state_input)
+		h1 = Dense(self.hidden_dim, activation = 'linear')(w1)
+
+		# parallel 2
+		action_input = Input(shape = [act_dim], name = 'action2')
+		a1 = Dense(self.hidden_dim, activation = 'linear')(action_input)
+
+		# merge
+		h2 = merge([h1, a1], mode = 'sum')
+		h3 = Dense(self.hidden_dim, activation = 'relu')(h2)
+		value_out = Dense(self.act_dim, activation = 'linear')(h3)
+
+		model = Model(input = [state_input, action_input], output = [value_out])
+		adam = Adam(lr = self.learning_rate)
+		model.compile(loss = 'mse', optimizer = adam)
+
+		return model, action_input, state_input
+
+
+	def gradients(self, states, acctions):
+		return self.sess.run(self.action_grads, feed_dict = {
+			self.state: states,
+			self.action: actions
+		})[0]
+
+	def target_train(self):
+		critic_weights = self.model.get_weights()
+		critic_target_weights = self.target_model.get_weights()
+
+		for i in xrange (len(critic_weights)):
+			critic_target_weights[i] = self.tau*critic_weights[i] + (1 - self.tau)*critic_target_weights[i]
+
+		self.target_model.set_weights(critic_target_weights)
--- rl/src/environment.py
+++ rl/src/environment.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import rospy
 from hector_uav_msgs.msg import Altimeter
 from std_msgs.msg import Header
@@ -7,102 +8,118 @@ from hector_uav_msgs.msg import Altimeter
 import message_filters
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
-import keras
 import gazeboInterface as gazebo
+import time
+import random
+import math
 
 class Environment():
 
-	def __init__(self):
+    def __init__(self):
 
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-
-		self.gazebo = gazebo()
-		self.running_step = 0.3  # Convert to ros param
-        self.max_incl = 0.7
-        self.max_altitude = 10.0
+        self.gazebo = gazebo.GazeboInterface()
+        self.running_step = 0.3  # Convert to ros param
+        self.max_incl = np.pi/2
+        self.max_altitude = 50.0
         self.vel_min = -2.0
         self.vel_max = 2.0
         self.goalPos = [0.0, -5.0, 2.0]
 
-	def _step(self, action):
-		# Input: action
-		# Output: nextState, reward, isTerminal, [] (not sending any debug information)
+        # imu_sub = message_filters.Subscriber("/raw_imu", Imu)
+        # pose_sub = message_filters.Subscriber("/ground_truth_to_tf/pose", PoseStamped)
+        # vel_sub = message_filters.Subscriber("/fix_velocity", Vector3Stamped)
+        
+        # fs = [imu_sub, pose_sub, vel_sub]
+        # queue_size = 2  # The both have a frquency of 100Hz, we won't need more than 2 messages of each
+        # slop = 0.07  # (in sec) The above two topics are by observation at max 0.05 out of sync
+        # ats = message_filters.ApproximateTimeSynchronizer(fs, queue_size, slop)  #, allow_headerless=False)
 
-		vel = Twist()
+        # ats.registerCallback(self.sensor_callback)
 
-		vel.linear.x = action[0]
+    def _step(self, action):
+
+        # Input: action
+        # Output: nextState, reward, isTerminal, [] (not sending any debug information)
+
+        vel = Twist()
+        vel.linear.x = action[0]
         vel.linear.y = action[1]
-		vel.linear.z = action[2]
+        vel.linear.z = action[2]
 
-        self.vel_pub.publish(vel)
+        self.gazebo.unpauseSim()
+        self.pub.publish(vel)
         time.sleep(self.running_step)
         poseData, imuData, velData = self.takeObservation()
         self.gazebo.pauseSim()
 
-        reward, isTerminal = self.processData(poseData, imuData, velData)
+        pose_ = poseData.pose
+        reward, isTerminal = self.processData(pose_, imuData, velData)
 
-        nextState = [poseData.position.x, poseData.position.y, poseData.position.z]
+        nextState = [pose_.position.x, pose_.position.y, pose_.position.z]
 
-		return nextState, reward, isTerminal, []
+        return nextState, reward, isTerminal, []
 
-	def _reset(self):
+    def _reset(self):
 
-        # 1st: resets the simulation to initial values
-        self.gazebo.resetSim()
+            # 1st: resets the simulation to initial values
+            self.gazebo.resetSim()
 
-        # 2nd: Unpauses simulation
-        self.gazebo.unpauseSim()
+            # 2nd: Unpauses simulation
+            self.gazebo.unpauseSim()
 
-        # 3rd: Don't want to start the agent from the ground
-        self.takeoff()
+            # 3rd: Don't want to start the agent from the ground
+            self.takeoff()
 
-        # 4th: Get init state
-        # TODO: Should initial state have some randomness?
-        initStateData, _, _ = self.takeObservation()
+            # 4th: Get init state
+            # TODO: Should initial state have some randomness?
+            initStateData, _, _ = self.takeObservation()
 
-        initState = [initStateData.position.x, initStateData.position.y, initStateData.position.z]
+            initState = [initStateData.pose.position.x, initStateData.pose.position.y, initStateData.pose.position.z]
 
-		return initState
+            # 5th: pauses simulation
+            self.gazebo.pauseSim()
+            
+            return initState
 
-	def _sample(self):
+    def _sample(self):
 
         vel_x = random.uniform(self.vel_min, self.vel_max)
         vel_y = random.uniform(self.vel_min, self.vel_max)
         vel_z = random.uniform(self.vel_min, self.vel_max)
 
-		return [vel_x, vel_y, vel_z]
+        return [vel_x, vel_y, vel_z]
 
-	def takeObservation(self):
-		# TODO: Using wait_for_message for now, might change to ApproxTimeSync later 
+    def takeObservation(self):
+        # TODO: Using wait_for_message for now, might change to ApproxTimeSync later 
 
         poseData = None
         while poseData is None:
-            try:
-                poseData = rospy.wait_for_message('/ground_truth_to_tf/pose', Pose, timeout=5)
-            except:
-                rospy.loginfo("Current drone pose not ready yet, retrying to get robot pose")
+          try:
+              poseData = rospy.wait_for_message('/ground_truth_to_tf/pose', PoseStamped, timeout=5)
+          except:
+              rospy.loginfo("Current drone pose not ready yet, retrying to get robot pose")
 
         velData = None
         while velData is None:
-            try:
-                velData = rospy.wait_for_message('/fix_velocity', Pose, timeout=5)
-            except:
-                rospy.loginfo("Current drone velocity not ready yet, retrying to get robot velocity")
+          try:
+              velData = rospy.wait_for_message('/fix_velocity', Vector3Stamped, timeout=5)
+          except:
+              rospy.loginfo("Current drone velocity not ready yet, retrying to get robot velocity")
 
         imuData = None
         while imuData is None:
-            try:
-                imuData = rospy.wait_for_message('/raw_imu', Imu, timeout=5)
-            except:
-                rospy.loginfo("Current drone imu not ready yet, retrying to get robot imu")
+          try:
+              imuData = rospy.wait_for_message('/raw_imu', Imu, timeout=5)
+          except:
+              rospy.loginfo("Current drone imu not ready yet, retrying to get robot imu")
         
         return poseData, imuData, velData
 
     def _distance(self, pose):
 
         currentPos = [pose.position.x, pose.position.y, pose.position.z]
-        dist = np.linalg.norm(currentPos - self.goalPos)
+        dist = np.linalg.norm(np.subtract(currentPos, self.goalPos))
         return dist
     
     def getReward(self, poseData, imuData, velData):
@@ -114,28 +131,52 @@ class Environment():
         error = self._distance(poseData)
         reward += -error
 
-        angletoGoal = np.arctan2(np.abs(poseData.position.y - goalPos[1]), np.abs(poseData.position.x - goalPos[2]))
+        angletoGoal = np.arctan2(np.abs(poseData.position.y - self.goalPos[1]), np.abs(poseData.position.x - self.goalPos[2]))
         currentAngle = np.arctan2(velData.vector.y, velData.vector.x)
 
-        if(-np.pi/6 < currentAngle < np.pi/6):
+        # Debug print statements
+        # print('arctan2({},{}), arctan2({},{})'.format(np.abs(poseData.position.y - self.goalPos[1]), np.abs(poseData.position.x - self.goalPos[2]), velData.vector.y, velData.vector.x))
+        # print('angletoGoal: {}, currentAngle: {}'.format(angletoGoal, currentAngle))
+
+        if(angletoGoal - np.pi/6 < currentAngle < angletoGoal + np.pi/6):
             reward += 1
         else:
             reward -= 5
 
         return reward
 
+    def quaternion_to_euler_angle(self, x, y, z, w):
+        ysqr = y * y
+        
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + ysqr)
+        X = math.atan2(t0, t1)
+        
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        Y = math.asin(t2)
+        
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (ysqr + z * z)
+        Z = math.atan2(t3, t4)
+        
+        return X, Y, Z
+
     def processData(self, poseData, imuData, velData):
 
         done = False
         
-        euler = tf.transformations.euler_from_quaternion([imuData.orientation.x, imuData.orientation.y, imuData.orientation.z, imuData.orientation.w])
-        roll = euler[0]
-        pitch = euler[1]
-        yaw = euler[2]
+        # euler = tf.transformations.euler_from_quaternion([imuData.orientation.x, imuData.orientation.y, imuData.orientation.z, imuData.orientation.w])
+        # roll = euler[0]
+        # pitch = euler[1]
+        # yaw = euler[2]
+
+        roll, pitch, yaw = self.quaternion_to_euler_angle(imuData.orientation.x, imuData.orientation.y, imuData.orientation.z, imuData.orientation.w)
 
         pitch_bad = not(-self.max_incl < pitch < self.max_incl)
         roll_bad = not(-self.max_incl < roll < self.max_incl)
-        altitude_bad = data_position.position.z > self.max_altitude
+        altitude_bad = poseData.position.z > self.max_altitude
 
         if altitude_bad or pitch_bad or roll_bad:
             rospy.loginfo ("(Terminating Episode: Unstable quad) >>> ("+str(altitude_bad)+","+str(pitch_bad)+","+str(roll_bad)+")")
@@ -155,11 +196,14 @@ class Environment():
         # while not rospy.is_shutdown():
         while count < 10:
             msg.linear.z = 0.5
-            rospy.loginfo('Lift off')
+            # rospy.loginfo('Lift off')
 
-            pub_.publish(msg)
+            self.pub.publish(msg)
             count = count + 1
             rate.sleep()
+
+        msg.linear.z = 0.0
+        self.pub.publish(msg)
 
         print('Take-off sequence completed')
         return
